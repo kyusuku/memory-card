e5bde374bd17544fb326cf3a4fbfa5f826c42342
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-04-20 20:47:03 +0700

    Finish basic page design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="montserrat-font p-6">
+      <div className="flex items-start justify-between">
+        <h1 className="mb-8 text-4xl font-bold text-gray-900">
+          Pokémon Memory Game
+        </h1>
+        <div className="text-gray-900">
+          <p>Score: 0</p>
+          <p>Best score: 0</p>
+        </div>
+      </div>
+      <p className="text-gray-900">
+        Get points by clicking on an image but don't click on any more than
+        once!
+      </p>
+    </div>
+  );
 }
 
 export default App;

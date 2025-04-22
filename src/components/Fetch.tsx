a7928d56eee37f3054866b218555182858c8c070
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-04-22 22:02:39 +0700

    Add error and load handling to Fetch component

--- src/components/Fetch.tsx
+++ src/components/Fetch.tsx
@@ -6,14 +6,29 @@ interface FetchProps {
 
 const Fetch = ({ name }: FetchProps) => {
   const [photo, setPhoto] = useState<string>("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, { mode: "cors" })
-      .then((res) => res.json())
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then((res) => {
+        if (res.status >= 400) {
+          throw new Error("server error");
+        }
+
+        return res.json();
+      })
       .then((data) => {
         setPhoto(data.sprites.front_default);
-      });
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
   }, [name]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    return <p>Error fetching image</p>;
+  }
 
   return <img key={name} src={photo} alt={name} />;
 };
